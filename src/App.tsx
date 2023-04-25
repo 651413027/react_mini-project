60aaf1f4f13c4ba50440f5d87f2da42338909c24
Your Name <you@example.com>
2023-04-25 17:05:48 +0700

    mini

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NotFound from './components/NotFound'
+import Home from './components/Home'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         </div>
         <div className='contianer'>
           <Routes>
+            <Route path='/' element={<Home />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
         </div>
